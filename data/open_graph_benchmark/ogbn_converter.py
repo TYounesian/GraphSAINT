3469f9f206aceaad4e37224ff499a041d7563d55
TYounesian <t.yoonesian@gmail.com>
2023-09-07 16:05:17 +0200

    update

--- data/open_graph_benchmark/ogbn_converter.py
+++ data/open_graph_benchmark/ogbn_converter.py
@@ -19,9 +19,9 @@ split_idx = dataset.get_idx_split()
 train_idx, valid_idx, test_idx = split_idx['train'], split_idx['valid'], split_idx['test']
 graph = dataset[0]
 num_node = graph.y.shape[0]
-# import pdb; pdb.set_trace()
+import pdb; pdb.set_trace()
 
-save_dir = './data/'+sys.argv[1]+'/'
+save_dir = './dataset/'+sys.argv[1]+'/'
 try:
     os.mkdir(save_dir)
 except OSError as error:
